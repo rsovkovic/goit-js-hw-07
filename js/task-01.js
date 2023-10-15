07308bb3f709e9b19393f2a2c16b2257d73445b1
rsovkovic <rsovkovic@gmail.com>
2023-10-15 10:09:46 +0300

    addCommit

--- js/task-01.js
+++
@@ -1,7 +0,0 @@
-const items = document.querySelectorAll(".item");
-console.log(items.length);
-items.forEach((item) => {
-  console.log(item.firstElementChild.textContent);
-  // console.log(item.querySelectorAll("li").length);
-  console.log(item.lastElementChild.children.length);
-});
--- js/task-07.js
+++
@@ -1,5 +0,0 @@
-const textInput = document.querySelector("#font-size-control");
-const spanValue = document.querySelector("#text");
-textInput.addEventListener("input", (event) => {
-  spanValue.style.fontSize = event.target.value + "px";
-});
